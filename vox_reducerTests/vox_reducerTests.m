e516d21c53525e7ce703749462436a326fd5db77
Bill S <bill.e.seaman@gmail.com>
2018-03-29 15:55:25 -0700

    Add some basic unit tests

--- vox_reducerTests/vox_reducerTests.m
+++ vox_reducerTests/vox_reducerTests.m
@@ -13,15 +13,13 @@
 @property (nonatomic) TargetViewController *vcToTest;
 @end
 
-
-
 @implementation vox_reducerTests
 
 - (void)setUp {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
 	
-	_vcToTest = [[TargetViewController alloc] init];
+	self.vcToTest = [[TargetViewController alloc] init];
 }
 
 - (void)tearDown {
@@ -29,8 +27,23 @@
     [super tearDown];
 }
 
-- (void)testExample {
+- (void)testSenderName {
+	NSString* name = @"testName";
+	self.vcToTest.senderName = name;
+	NSString* retName = self.vcToTest.senderName;
+	
+	//Just to confirm unit test is working properly
+	NSString* expectedName = @"testNameWrong";
 
+	//The XCTAssertEqualObjects function is part of the XCTest framework. The XCTest framework provides many other methods to make assertions about application state, such as variable equality or boolean expression results
+	XCTAssertEqualObjects(expectedName, retName, @"strings did not match");
+}
+
+//Performance test - time it take to complete call
+- (void)testPerformanceSender {
+	[self measureBlock:^{
+		self.vcToTest.senderName;
+	}];
 }
 
 
